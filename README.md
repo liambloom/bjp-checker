b2f2de34161e0aaf86f7dfff99905fc2e0b7c552
liambloom <liamrbloom@gmail.com>
2021-03-10 19:12:30 -0500

    I'm probably going to remove maven

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 One of the dependencies is a local dependency. This program requires the Xerces version that supports XML Schema 1.1, which the version on maven doesn't. Instead, if you want to use the source code, you need to download the version that does support it, which you can do [here](http://xerces.apache.org/mirrors.cgi#binary).
 
 [Here](https://stackoverflow.com/a/4955695/11326662) are instructions on how to add a local jar file to your maven repository.
+
+Keep in mind that the Xerces jar is not an &uuml;ber jar, so you will also need to ensure you provide its dependencies
--- src/main/java/io/github/liambloom/tests/book/bjp3/TestLoader.java
+++ src/main/java/io/github/liambloom/tests/book/bjp3/TestLoader.java
@@ -8,15 +8,20 @@ import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class TestLoader {
-    protected final static Schema schema;
+    protected static final Schema schema;
 
     static {
         Schema s;
         try {
-            // FIXME: It's not finding the xerces xsd 1.1 parser
-            s = SchemaFactory.newInstance("http://www.w3.org/XML/XMLSchema/v1.1").newSchema(new StreamSource(TestLoader.class.getResourceAsStream("../book-tests.xsd")));
+            // FIXME: book-tests.xsd might not be ending up in the jar
+            InputStream stream = TestLoader.class.getResourceAsStream("../../../../../../book-tests.xsd");
+            /* System.out.write(stream.readAllBytes());
+            System.out.println(); */
+            s = SchemaFactory.newInstance("http://www.w3.org/XML/XMLSchema/v1.1").newSchema(new StreamSource(stream));
         } catch (SAXException e) {
             e.printStackTrace();
             s = null;
