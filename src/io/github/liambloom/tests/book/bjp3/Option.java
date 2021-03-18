97bfc3d17bf2fa9639da5b88cf0b81e26ae49246
liambloom <liamrbloom@gmail.com>
2021-03-17 22:43:54 -0400

    Create Option.java
    I probably won't use this

---
+++ src/io/github/liambloom/tests/book/bjp3/Option.java
@@ -0,0 +1,19 @@
+package io.github.liambloom.tests.book.bjp3;
+
+abstract class Option<T> {
+    private final T value;
+
+    protected Option(T value) {
+        this.value = value;
+    }
+
+    public abstract String name();
+    public abstract String help();
+    public T value() {
+        return value;
+    }
+    public String[] flags() {
+        return new String[]{ "-" + name().charAt(0), "--" + name() };
+    }
+    // Class<T> type();
+}
