53361a80ccc9870614bbda7baf4c0c5179e13c1d
liambloom <liamrbloom@gmail.com>
2021-03-17 22:43:41 -0400

    Worked on arguments

--- src/io/github/liambloom/tests/book/bjp3/Arguments.java
+++ src/io/github/liambloom/tests/book/bjp3/Arguments.java
@@ -6,10 +6,12 @@ import java.util.Map;
 
 // Add placeholder values for now
 class Arguments {
-    public final int chapter = 1;
-    public final Integer exercise = 1; // I need an "all" value. I need union types
-    public final Integer programmingProject = null;
-    public final XMLCheckLevel xmlCheckLevel = XMLCheckLevel.TargetOnlyCheck;
+    public int chapter = -1;
+    public final Integer exercise; // I need an "all" value. I need union types
+    public final Integer programmingProject;
+    public final XMLCheckLevel xmlCheckLevel;
+    public boolean debug = false;
+    public final String[] commands;
 
     //public final File targetDir;
 
@@ -18,7 +20,7 @@ class Arguments {
         // Don't make things final, initialize them to null
         // Deal with defaults on access
         // Maybe do different things based on the command
-        /*if (args.length == 0) {
+        if (args.length == 0) {
             this.commands = new String[] { "--help" };
             return;
         }
@@ -28,8 +30,36 @@ class Arguments {
 
         for (; i < args.length && (!args[i].startsWith("-") || args[i].equals("--help")); i++)
             commands.add(args[i]);
-        this.commands = commands.toArray(new String[0]);*/
+        this.commands = commands.toArray(new String[0]);
 
+        for (; i < args.length; i++) {
+            if (!args[i].startsWith("-") || args[i].equals("--help"))
+                throw new UserErrorException("Unexpected subcommand " + args[i] + ", expected flag");
+            switch (args[i]) {
+                case "-d":
+                case "--debug":
+                    if (debug)
+                        throw new UserErrorException("Duplicate argument: " + args[i]);
+                    debug = true;
+                    break;
+                case "-c":
+                case "--chapter":
+                    try {
+                        if (chapter != -1)
+                            throw new UserErrorException("Duplicate argument: " + args[i]);
+                        chapter = Integer.parseInt(args[++i]);
+                        // Don't check range because other chapters may be defined in tests
+                    }
+                    catch (NumberFormatException e) {
+                        throw new UserErrorException("Expected integer, found\"" + args[i] + '"', e);
+                    }
+                    break;
+                case "-e":
+                case "--exercise":
+                case "--exercises":
+
+            }
+        }
         // TODO: Parse other flags (this may be problematic if flags are command-dependent)
     }
 
---
+++ src/io/github/liambloom/tests/book/bjp3/Ranges.java
@@ -0,0 +1,352 @@
+package io.github.liambloom.tests.book.bjp3;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+/**
+ * Represents a {@link Collection} of ranges.
+ *
+ * This is represented internally with a tree.
+ */
+class Ranges {
+    private Range value;
+    private Ranges left;
+    private Ranges right;
+
+    public Collection<Integer> asIntegerCollection() {
+        return new Collection<Integer>() {
+            @Override
+            public boolean add(Integer e) {
+                return Ranges.this.asRangeCollection().add(new Range(e));
+            }
+
+            @Override
+            public boolean addAll(Collection<? extends Integer> c) {
+                boolean r = false;
+                for (Integer e : c) {
+                    if (add(e))
+                        r = true;
+                }
+                return r;
+            }
+
+            @Override
+            public boolean remove(Object o) {
+                return Ranges.this.remove(o);
+            }
+
+            @Override
+            public boolean removeAll(Collection<?> c) {
+                return Ranges.this.removeAll(c);
+            }
+
+            @Override
+            public boolean retainAll(Collection<?> c) {
+                return Ranges.this.retainAll(c);
+            }
+
+            @Override
+            public void clear() {
+                Ranges.this.clear();
+            }
+
+            @Override
+            public boolean contains(Object o) {
+                return Ranges.this.contains(o);
+            }
+
+            @Override
+            public boolean containsAll(Collection<?> c) {
+                return Ranges.this.containsAll(c);
+            }
+
+            @Override
+            public int size() {
+                // TODO
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return Ranges.this.isEmpty();
+            }
+
+            @Override
+            public Iterator<Integer> iterator() {
+                // TODO
+            }
+
+            @Override
+            public Object[] toArray() {
+                return toArray(new Object[0]);
+            }
+
+            @Override
+            public <T> T[] toArray(T[] a) {
+                // TODO
+            }
+
+            @Override
+            public String toString() {
+                return Ranges.this.toString();
+            }
+
+            @Override
+            public boolean equals(Object o) {
+                return Ranges.this.equals(o);
+            }
+        };
+    }
+
+    public Collection<Range> asRangeCollection() {
+        return new Collection<Range>() {
+            @Override
+            public boolean add(Range r) {
+                if (value == null){
+                    value = r;
+                    return true;
+                }
+                else if (value.contains(r))
+                    return false;
+                else if (r.joinable(value)) {
+                    value = value.join(r);
+                    if (left != null) {
+                        value = left.tryJoinRight(value);
+                        if (left.value == null)
+                            left = null;
+                    }
+                    if (right != null) {
+                        value = right.tryJoinLeft(value);
+                        if (right.value == null)
+                            right = null;
+                    }
+                    return true;
+                }
+                else if (r.max < value.min)
+                    return left.asRangeCollection().add(r);
+                else if (r.min > value.max)
+                    return right.asRangeCollection().add(r);
+                else
+                    throw new IllegalStateException("A mistake was made when comparing ranges");
+            }
+
+            @Override
+            public boolean addAll(Collection<? extends Range> c) {
+                boolean r = false;
+                for (Range e : c) {
+                    if (add(e))
+                        r = true;
+                }
+                return r;
+            }
+
+            @Override
+            public boolean remove(Object o) {
+                return Ranges.this.remove(o);
+            }
+
+            @Override
+            public boolean removeAll(Collection<?> c) {
+                return Ranges.this.removeAll(c);
+            }
+
+            @Override
+            public boolean retainAll(Collection<?> c) {
+                return Ranges.this.retainAll(c);
+            }
+
+            @Override
+            public void clear() {
+                Ranges.this.clear();
+            }
+
+            @Override
+            public boolean contains(Object o) {
+                return Ranges.this.contains(o);
+            }
+
+            @Override
+            public boolean containsAll(Collection<?> c) {
+                return Ranges.this.containsAll(c);
+            }
+
+            @Override
+            public int size() {
+                // TODO
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return Ranges.this.isEmpty();
+            }
+
+            @Override
+            public Iterator<Range> iterator() {
+                // TODO
+            }
+
+            @Override
+            public Object[] toArray() {
+                return toArray(new Object[0]);
+            }
+
+            @Override
+            public <T> T[] toArray(T[] a) {
+                // TODO
+            }
+
+            @Override
+            public String toString() {
+                return Ranges.this.toString();
+            }
+
+            @Override
+            public boolean equals(Object o) {
+                return Ranges.this.equals(o);
+            }
+        };
+    }
+
+    /**
+     * Tries to join this {@code Range} with the one passed in, which should
+     * be to its right. Calling this method is does require some work on the
+     * part of the callers, as it implements the {@code x = change(x)} pattern,
+     * and needs to be able to set itself to null. An example usage would be:
+     *
+     * <pre>
+     * {@code
+     * value = left.tryJoinRight(value);
+     * if (left.value == null)
+     *     left = null;
+     * }
+     * </pre>
+     *
+     * @param root The element to the right of {@code this} to join
+     * @return The new value of {@code root}
+     */
+    private Range tryJoinRight(Range root) {
+        if (root.joinable(value)) {
+            root = root.join(value);
+
+            // this = left
+            if (left == null)
+                value = null;
+            else {
+                value = left.value;
+                right = left.right;
+                left = left.left;
+            }
+        }
+        else if (right != null) {
+            root = right.tryJoinRight(root);
+            if (right.value == null)
+                right = null;
+        }
+
+        return root;
+    }
+
+    /**
+     * Tries to join this {@code Range} with the one passed in, which should
+     * be to its left. Calling this method is does require some work on the
+     * part of the callers, as it implements the {@code x = change(x)} pattern,
+     * and needs to be able to set itself to null. An example usage would be:
+     *
+     * <pre>
+     * {@code
+     * value = right.tryJoinLeft(value);
+     * if (right.value == null)
+     *     right = null;
+     * }
+     * </pre>
+     *
+     * @param root The element to the left of {@code this} to join
+     * @return The new value of {@code root}
+     */
+    private Range tryJoinLeft(Range root) {
+        if (root.joinable(value)) {
+            root = root.join(value);
+
+            // this = left
+            if (right == null)
+                value = null;
+            else {
+                value = right.value;
+                left = right.left;
+                right = right.right;
+            }
+        }
+        else if (left != null) {
+            root = left.tryJoinRight(root);
+            if (left.value == null)
+                left = null;
+        }
+
+        return root;
+    }
+
+    public boolean remove(Object o) {
+        // TODO
+    }
+
+    public boolean removeAll(Collection<?> o) {
+        // TODO
+    }
+
+    public boolean retainAll(Collection<?> o) {
+        // TODO
+    }
+
+    public void clear() {
+        this.value = null;
+        this.left = null;
+        this.right = null;
+    }
+
+    public boolean contains(Object o) {
+        // TODO
+    }
+
+    public boolean containsAll(Collection<?> c) {
+        // TODO
+    }
+
+    public boolean isEmpty() {
+        return value != null;
+    }
+
+    /**
+     * @return The minimum value in the minimum range
+     * @throws IllegalStateException if range is empty
+     */
+    public Integer min() {
+        if (value == null)
+            throw new IllegalStateException("Cannot call min on empty range");
+        if (left == null)
+            return value.min;
+        else
+            return left.min();
+    }
+
+    /**
+     * @return The maximum value in the minimum range
+     * @throws IllegalStateException if range is empty
+     */
+    public Integer max() {
+        if (value == null)
+            throw new IllegalStateException("Cannot call min on empty range");
+        if (right == null)
+            return value.max;
+        else
+            return right.max();
+    }
+
+    @Override
+    public String toString() {
+        // TODO
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        // TODO
+    }
+}
