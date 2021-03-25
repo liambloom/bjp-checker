5c38c455702b51aebc8939dc0bec29f62b92e691
liambloom <liamrbloom@gmail.com>
2021-03-25 11:37:40 -0400

    Removed some try-catch statements

--- src/io/github/liambloom/tests/book/bjp3/DirectoryClassLoader.java
+++ src/io/github/liambloom/tests/book/bjp3/DirectoryClassLoader.java
@@ -12,27 +12,22 @@ import java.util.regex.Pattern;
 class DirectoryClassLoader {
     private static final Pattern PATH_SEGMENT = Pattern.compile("\\" + File.separatorChar + "[a-zA-Z_$][\\w$]*");
 
-    public static Class<?>[] loadClassesHere() {
+    public static Class<?>[] loadClassesHere() throws ClassNotFoundException, MalformedURLException {
         return loadClasses(new File(System.getProperty("user.dir")));
     }
 
-    public static Class<?>[] loadClasses(File here) {
+    public static Class<?>[] loadClasses(File here) throws ClassNotFoundException, MalformedURLException {
         List<String> files = new LinkedList<>();
         List<URL> dirs = new LinkedList<>();
         getFiles(here, files, dirs);
-        try {
-            String prev = "";
-            File parent = here.getParentFile();
-            while (parent != null) {
-                if (!PATH_SEGMENT.matcher(prev.replace(parent.toString(), "")).matches())
-                    break;
-                dirs.add(parent.toURI().toURL());
-                prev = parent.toString();
-                parent = parent.getParentFile();
-            }
-        }
-        catch (MalformedURLException e) {
-            // This should not be thrown, see below.
+        String prev = "";
+        File parent = here.getParentFile();
+        while (parent != null) {
+            if (!PATH_SEGMENT.matcher(prev.replace(parent.toString(), "")).matches())
+                break;
+            dirs.add(parent.toURI().toURL());
+            prev = parent.toString();
+            parent = parent.getParentFile();
         }
 
         // Gets class loaders
@@ -47,41 +42,29 @@ class DirectoryClassLoader {
         for (int i = 0; fileIter.hasNext(); i++) {
             String file = fileIter.next();
             for (URLClassLoader loader : loaders) {
-                try {
-                    // FIXME: Doesn't work with nested/inner classes
-                    classes[i] = loader.loadClass(file.replace(".class", "").replace(loader.getURLs()[0].toString(), "").replace("/", "."));
-                    break;
-                }
-                catch (ClassNotFoundException | NoClassDefFoundError e) {
-                    Main.app.debugger.internalError(e);
-                }
+                // FIXME: Doesn't work with nested/inner classes
+                classes[i] = loader.loadClass(file.replace(".class", "").replace(loader.getURLs()[0].toString(), "").replace("/", "."));
+                break;
             }
             assert classes[i] != null;
         }
 
         return classes;
-    } // mon 1:00
+    }
 
-    private static void getFiles(final File parent, final List<String> files, final List<URL> dirs) {
-        try {
-            if (parent.isFile())
-                files.add(parent.toURI().toURL().toString());
-            else {
-                int len = files.size();
-                for (File f : parent.listFiles()) {
-                    if (f.isDirectory())
-                        getFiles(f, files, dirs);
-                    else if (f.getName().endsWith(".class"))
-                        files.add(f.toURI().toURL().toString());
-                }
-                if (len != files.size())
-                    dirs.add(parent.toURI().toURL());
-            } 
-        }
-        catch (MalformedURLException e) {
-            // A MalformedURLException is thrown if
-            // - No legal protocol is found. This won't happen, since "file" is a legal protocol
-            // - The string could not be parsed. I dont' think this can happen either, but honestly I have no idea
+    private static void getFiles(final File parent, final List<String> files, final List<URL> dirs) throws MalformedURLException {
+        if (parent.isFile())
+            files.add(parent.toURI().toURL().toString());
+        else {
+            int len = files.size();
+            for (File f : parent.listFiles()) {
+                if (f.isDirectory())
+                    getFiles(f, files, dirs);
+                else if (f.getName().endsWith(".class"))
+                    files.add(f.toURI().toURL().toString());
+            }
+            if (len != files.size())
+                dirs.add(parent.toURI().toURL());
         }
     }
 }
