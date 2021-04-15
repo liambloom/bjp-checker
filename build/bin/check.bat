b8f4ad357756fcc4ec511c04dee0a7e46ad4412f
liambloom <liamrbloom@gmail.com>
2021-04-14 23:19:43 -0400

    Replaced batch file with executable

--- build/bin/check.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-:: Note: %~dp0 doesn't work with symlinks, but that's not a standard or important thing on windows.
-:: https://superuser.com/q/927411/1158654
-
-if "%CHECKER_DEBUG%" == "1" echo Debug mode is ON
-
-java -cp "%~dp0..\lib\*" io.github.liambloom.tests.book.bjp3.App %*
