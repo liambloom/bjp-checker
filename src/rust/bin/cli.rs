28015a12c9a54a49daf10d74311df39411117632
liambloom <liamrbloom@gmail.com>
2021-05-01 17:19:26 -0400

    Cleaned up cli binary

--- src/rust/bin/cli.rs
+++ src/rust/bin/cli.rs
@@ -14,17 +14,13 @@ fn main() {
 
 #[cfg(windows)]
 fn print_error(e: impl Display) {
-    use winapi::um::wincon::{FOREGROUND_RED, GetConsoleScreenBufferInfo, SetConsoleTextAttribute, CONSOLE_SCREEN_BUFFER_INFO, CreateConsoleScreenBuffer, CONSOLE_TEXTMODE_BUFFER};
-    use winapi::um::winnt::{GENERIC_READ, FILE_SHARE_READ, FILE_SHARE_WRITE};
-    use winapi::um::processenv::SetStdHandle;
-    use winapi::um::winbase::STD_ERROR_HANDLE;
+    use winapi::um::wincon::{FOREGROUND_RED, GetConsoleScreenBufferInfo, SetConsoleTextAttribute, CONSOLE_SCREEN_BUFFER_INFO};
     use winapi::um::wincontypes::{COORD, SMALL_RECT};
-    use std::os::windows::io::{AsRawHandle, FromRawHandle};
-    use std::io::{stdout, stderr, Write};
-    use std::ptr::{null, null_mut};
-    use std::default::Default;
+    use winapi::um::errhandlingapi::GetLastError;
+    use std::os::windows::io::AsRawHandle;
+    use std::io::stderr;
+    use std::process::exit;
 
-    //stderr().as_raw_handle() as *mut winapi::ctypes::c_void
     let mut console_info = CONSOLE_SCREEN_BUFFER_INFO {
         dwSize: COORD { X: 0, Y: 0 },
         dwCursorPosition: COORD { X: 0, Y: 0 },
@@ -38,40 +34,26 @@ fn print_error(e: impl Display) {
         dwMaximumWindowSize: COORD { X: 0, Y: 0 }
     };
 
-    println!("before");
+    let handle = stderr().as_raw_handle() as *mut winapi::ctypes::c_void;
+
     unsafe {
-        // let handle = CreateConsoleScreenBuffer(GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE, null(), CONSOLE_TEXTMODE_BUFFER, null_mut());//winapi::um::processenv::GetStdHandle(winapi::um::winbase::STD_ERROR_HANDLE);
-        // SetStdHandle(STD_ERROR_HANDLE, handle);
-        eprintln!("test");
-
-        if 0 == GetConsoleScreenBufferInfo(stderr().as_raw_handle() as *mut winapi::ctypes::c_void, &mut console_info as *mut CONSOLE_SCREEN_BUFFER_INFO) {
-            println!("{}", winapi::um::errhandlingapi::GetLastError());
-            println!("failed to get");
-        }
-        else {
-            println!("get succeeded")
-        }
-
-        stderr().flush();
-
-        eprintln!("uh-oh");
-
+        if GetConsoleScreenBufferInfo(handle, &mut console_info) == 0 { exit(GetLastError() as i32); }
         let saved_attributes = console_info.wAttributes;
-        if 0 == SetConsoleTextAttribute(stderr().as_raw_handle() as *mut winapi::ctypes::c_void, FOREGROUND_RED) {
-            println!("{}", winapi::um::errhandlingapi::GetLastError());
-            println!("failed to set");
-        }
-        else {
-            println!("set succeeded")
-        }
+
+        if SetConsoleTextAttribute(handle, FOREGROUND_RED) == 0 { exit(GetLastError() as i32); }
         eprint!("[error] ");
-         SetConsoleTextAttribute(stderr().as_raw_handle() as *mut winapi::ctypes::c_void, saved_attributes);
-        eprintln!("{}", e);
+        if SetConsoleTextAttribute(handle, saved_attributes) == 0 { exit(GetLastError() as i32); }
     }
-    println!("after");
+
+    eprintln!("{}", e);
 }
 
-#[cfg(not(windows))]
+#[cfg(unix)]
 fn print_error(e: impl Display) {
-    eprintln!("\x1b[31m[error]\x1b[0m {}", e)
+    eprintln!("\x1b[31m[error]\x1b[0m {}", e);
+}
+
+#[cfg(all(not(windows), not(unix)))]
+fn print_error(e: impl Display) {
+    eprintln!("[error] {}", e);
 }
