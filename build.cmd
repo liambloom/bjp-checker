ceaba6f6843c30a1a2517294b65e0b05460551bb
liambloom <liamrbloom@gmail.com>
2021-05-11 22:18:48 -0400

    Changed rust build output dir

--- build.cmd
+++ build.cmd
@@ -1,20 +1,21 @@
 @echo off
 goto :%1
 
-rem I should be using intellij build tools.
-rem But I couldn't set them up the way I wanted, and I couldn't get them back to how the were after I gave up.
-rem So here's a batch file that builds stuff
+rem This should do the same thing as my local intellij settings
 
 :bins
+cd rust
 cargo +nightly build --bin checker --release --features cli -Z unstable-options
 cargo +nightly build --bin checker-gui --release --features gui -Z unstable-options
+cd ..
 rem build for other OSes?
 exit /b
 
 :lib
-javac -sourcepath src\java -d out\java\production\bjp3-tests -source 8 -target 8 -cp build\lib\* -bootclasspath "C:\Program Files\Java\jdk1.8.0_281\jre\lib\rt.jar" src\java\dev\liambloom\tests\book\bjp\*.java src\java\dev\liambloom\tests\book\bjp\checker\*.java
-xcopy /q /y /s rsc\* out\java\production\bjp3-tests\ >nul
-jar cf build\lib\checker.jar -C out\java\production\bjp3-tests .
+
+javac -sourcepath java\src -d out\production\java -source 8 -target 8 -cp build\lib\* -bootclasspath "C:\Program Files\Java\jdk1.8.0_281\jre\lib\rt.jar" java\src\dev\liambloom\tests\book\bjp\*.java java\src\dev\liambloom\tests\book\bjp\checker\*.java
+xcopy /q /y /s java\rsc\* out\production\java >nul
+jar cf build\lib\checker.jar -C out\production\java .
 exit /b
 
 :docs
