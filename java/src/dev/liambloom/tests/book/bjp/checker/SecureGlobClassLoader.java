224977385f725b20edd5598bd62681e7366f9f66
liambloom <liamrbloom@gmail.com>
2021-05-24 22:47:48 -0400

    Even better ClassLoader
    I came up with an even better way to implement the classloader (one that feels right and actually works how a classloader is designed to work), and fully implemented it (It could use some slight improvements, but it works)

--- java/src/dev/liambloom/tests/book/bjp/checker/SecureGlobClassLoader.java
+++ java/src/dev/liambloom/tests/book/bjp/checker/SecureGlobClassLoader.java
@@ -1,49 +1,40 @@
 package dev.liambloom.tests.book.bjp.checker;
 
 import java.io.*;
-import java.nio.ByteBuffer;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-import java.util.function.Predicate;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
 
 public final class SecureGlobClassLoader extends ClassLoader {
-    private final Lock lock = new ReentrantLock();
-    private final Map<String, Condition> loadConditions = Collections.synchronizedMap(new HashMap<>());
-    private boolean isLoading = false;
-    private final Glob glob;
+    static final int JAVA_VERSION;
 
-    public SecureGlobClassLoader(Glob glob) {
+    static {
+        String vStr = System.getProperty("java.version");
+        int vDot = vStr.indexOf('.');
+        // v = The version number, or 1 if the version is <=8
+        JAVA_VERSION = Integer.parseInt(vDot == -1 ? vStr : vStr.substring(0, vDot));
+    }
+
+    private final SortedMap<String, LazyClass> classes;
+
+    public SecureGlobClassLoader(Glob glob) throws IOException {
         this(glob, getSystemClassLoader());
     }
 
-    public SecureGlobClassLoader(Glob glob, ClassLoader parent) {
+    public SecureGlobClassLoader(Glob glob, ClassLoader parent) throws IOException {
         super(parent);
-        this.glob = glob;
-    }
-
-    public Class<?>[] loadAll() throws IOException {
-        Queue<byte[]> q1;
         try {
-             q1 = glob.files()
+            classes = glob.files()
                     .map(File::toPath)
                     .map((FunctionThrowsIOException<Path, Path>) Glob::readSymbolicLink)
-                    .flatMap((FunctionThrowsIOException<Path, Stream<byte[]>>) (p -> {
+                    .flatMap((FunctionThrowsIOException<Path, Stream<ClassSource>>) (p -> {
                         if (p.toString().endsWith(".jar")) {
                             // TODO: Test if this works with MRJARs (it should, but it's untested)
                             JarFile jar = new JarFile(p.toFile());
-                            Stream<JarEntry> classFiles;
+                            Stream<ClassSource> classFiles;
                             String vStr = System.getProperty("java.version");
                             int vDot = vStr.indexOf('.');
                             // v = The version number, or 1 if the version is <=8
@@ -53,7 +44,7 @@ public final class SecureGlobClassLoader extends ClassLoader {
                                     .map("true"::equals)
                                     .orElse(false))
                             {
-                                Map<String, Tuple2<JarEntry, Integer>> entryMap = new HashMap<>();
+                                Map<String, ClassSource> entryMap = new HashMap<>();
                                 Enumeration<JarEntry> entries = jar.entries();
                                 while (entries.hasMoreElements()) {
                                     JarEntry entry = entries.nextElement();
@@ -64,89 +55,127 @@ public final class SecureGlobClassLoader extends ClassLoader {
                                         if (name.substring(9, 17).equalsIgnoreCase("version/")) {
                                             int ev = Integer.parseInt(name.substring(17, name.indexOf('/', 17)));
                                             if (ev <= v)
-                                                entryMap.compute(name, (k, val) -> val == null || val.e1() < ev ? new Tuple2<>(entry, ev) : val);
+                                                entryMap.compute(name.substring(name.indexOf('/', 17)), (BiFunctionThrowsIOException<String, ClassSource, ClassSource>)
+                                                        (k, val) -> val == null || val.version() < ev ? new ClassSource(k.substring(0, k.length() - 6).replace('/', '.'), jar.getInputStream(entry), ev) : val);
                                         }
                                     }
                                     else
-                                        entryMap.put(name, new Tuple2<>(entry, 8));
+                                        entryMap.put(name, new ClassSource(name.substring(0, name.length() - 6).replace('/', '.'), jar.getInputStream(entry), 8));
                                 }
-                                classFiles = entryMap.values().stream().map(Tuple2::e0);
+                                classFiles = entryMap.values().stream();
                             }
                             else {
                                 classFiles = jar.stream()
-                                        .filter(e -> !e.isDirectory() && e.getName().toLowerCase().endsWith(".class") && !e.getName().toUpperCase().startsWith("META-INF"));
+                                        .filter(e -> !e.isDirectory() && e.getName().toLowerCase().endsWith(".class") && !e.getName().toUpperCase().startsWith("META-INF"))
+                                        .map((FunctionThrowsIOException<JarEntry, ClassSource>)
+                                                (e -> new ClassSource(e.getName().substring(0, e.getName().length() - 6).replace('/', '.'), jar.getInputStream(e), 8)));
                             }
 
-                            return classFiles
-                                    .map((FunctionThrowsIOException<JarEntry, byte[]>) (f -> {
-                                        InputStream in = jar.getInputStream(f);
-                                        ByteArrayOutputStream bufs = new ByteArrayOutputStream();
-                                        byte[] buf = new byte[0x400]; // 1kb
-                                        int len;
-                                        while ((len = in.read(buf)) != -1)
-                                            bufs.write(buf, 0, len);
-                                        return bufs.toByteArray();
-                                    }));
+                            return classFiles;
                         }
-                        else if (p.toString().endsWith(".class")) {
-                            return Stream.of(Files.readAllBytes(p));
+                        else if (p.toString().endsWith(".class")){
+                            String abs = p.toAbsolutePath().toString();
+                            return Stream.of(new ClassSource(abs.substring(0, abs.length() - 6).replace(File.separatorChar, '.'), new FileInputStream(p.toFile())));
                         }
                         else
-                            // Somewhere, somehow, this gets wrapped in another UserErrorException (I think). Nevermind, it's fine now
+                            // Somewhere, somehow, this gets wrapped in another UserErrorException (I think).
+                            // Nevermind, it's fine now
                             throw new UserErrorException("Unable to load classes from `" + p + "' because it is not a .class or .jar file");
                     }))
-                    .collect(Collectors.toCollection(LinkedList::new));
+            .collect(Collectors.toMap(
+                    s -> new StringBuilder(s.name()).reverse().toString(),
+                    s -> new LazyClass(s.stream()),
+                    (v1, v2) -> { throw new RuntimeException(String.format("Duplicate key for values %s and %s", v1, v2)); },
+                    TreeMap<String, LazyClass>::new));
         }
         catch (UncheckedIOException e) {
             throw e.getCause();
         }
+    }
 
-        Queue<byte[]> q2 = new LinkedList<>();
-
-        Class<?>[] classes = new Class<?>[q1.size()];
-
-        int i = 0;
-        while (!q1.isEmpty()) {
-            boolean success = false;
-            NoClassDefFoundError failCause = null;
-            while (!q1.isEmpty()) {
-                byte[] b = q1.remove();
-                try {
-                    classes[i] = defineClass(null, b, 0, b.length);
-                    i++;
-                    success = true;
-                }
-                catch (NoClassDefFoundError e) {
-                    q2.add(b);
-                    failCause = e;
-                }
-                catch (LinkageError e) {
-                    throw new UserErrorException(e);
-                }
-            }
-            if (!success)
-                throw new UserErrorException(failCause);
-            Queue<byte[]> temp = q1;
-            q1 = q2;
-            q2 = temp;
-        }
-
-        return classes;
+    public Class<?>[] loadAll() {
+        return classes.values()
+                .stream()
+                .map((FunctionThrowsIOException<LazyClass, Class<?>>) c -> c.get(null))
+                .toArray(Class<?>[]::new);
     }
 
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
-        lock.lock();
-        if (isLoading) {
+        StringBuilder nameMut = new StringBuilder(name).reverse();
+        String from = nameMut.toString();
+        nameMut.setCharAt(name.length() - 1, (char) (nameMut.charAt(name.length() - 1) + 1));
+        String to = nameMut.toString();
+        for (LazyClass clazz : classes.subMap(from, to).values()) {
             try {
-                loadConditions.computeIfAbsent(name, k -> lock.newCondition()).await();
+                return clazz.get(name);
             }
-            catch (InterruptedException e) {
-                throw new RuntimeException(e);
+            catch (IOException e) {
+                throw new UncheckedIOException(e);
             }
-            return loadClass(name);
+            catch (NoClassDefFoundError ignored) {}
         }
-        else
-            return super.findClass(name);
+        return super.findClass(name);
+    }
+
+    private class LazyClass {
+        private Class<?> clazz = null;
+        private InputStream stream;
+
+        public LazyClass(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public Class<?> get(String name) throws IOException {
+            if (clazz == null) {
+                ByteArrayOutputStream bufs = new ByteArrayOutputStream();
+                byte[] buf = new byte[0x400]; // 1kb
+                int len;
+                while ((len = stream.read(buf)) != -1)
+                    bufs.write(buf, 0, len);
+                clazz = defineClass(name, bufs.toByteArray(), 0, bufs.size());
+                stream = null;
+            }
+            else if (name != null && !clazz.getName().equals(name))
+                throw new NoClassDefFoundError(name);
+            return clazz;
+        }
     }
 }
+
+class ClassSource {
+    private final int version;
+    private final InputStream stream;
+    private final String name;
+
+    // TODO: Add better constructors
+
+    /*public ClassSource(File f) throws FileNotFoundException {
+        this(f.getAbsolutePath(), new FileInputStream(f));
+    }*/
+
+    public ClassSource(String name, InputStream stream) {
+        this(name, stream, 8);
+    }
+
+    public ClassSource(String name, InputStream stream, int version) {
+        /*if (name.endsWith(".class"))
+            name = name.substring(0, name.length() - 6);*/
+        this.name = name;
+        this.stream = stream;
+        this.version = version;
+    }
+
+    public int version() {
+        return version;
+    }
+
+    public InputStream stream() {
+        return stream;
+    }
+
+    public String name() {
+        return name;
+    }
+}
+
