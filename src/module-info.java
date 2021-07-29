46501c3ed874762bc8160ab25da24db4f547ee36
liambloom <liamrbloom@gmail.com>
2021-07-29 16:40:03 -0400

    Revert "No longer in a module"
    This reverts commit 886b2708ad07fff5d7e2bc5bba82f3daf03ea367.

---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+module dev.liambloom.tests.bjp {
+    requires java.xml;
+    requires java.prefs;
+    requires java.compiler;
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires org.fusesource.jansi;
+    exports dev.liambloom.tests.bjp;
+    exports dev.liambloom.tests.bjp.gui to javafx.graphics;
+}
