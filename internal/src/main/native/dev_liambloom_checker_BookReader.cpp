c728605f04a9810d4483bbc6788dfeb18ddf00b9
liambloom <liamrbloom@gmail.com>
2021-10-31 00:20:12 -0400

    I compiled the c++ code!

--- internal/src/main/native/dev_liambloom_checker_BookReader.cpp
+++ internal/src/main/native/dev_liambloom_checker_BookReader.cpp
@@ -3,6 +3,7 @@
 
 JNIEXPORT void JNICALL Java_dev_liambloom_checker_BookReader_changeDirectory
   (JNIEnv* env, jclass thisClass, jstring str) {
-  const std::filesystem::path path = str;
-  std::filesystem::current_path(path)
+  const char* strChars = env->GetStringUTFChars(str, NULL);
+  const std::filesystem::path path = strChars;
+  std::filesystem::current_path(path);
 }
